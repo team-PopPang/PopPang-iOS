a9f61accfd60671ba6982c68f5bb74e78517c1a2
indextrown <indextrown@gmail.com>
2025-09-07 18:19:28 +0900

    chore: 프로젝트 세팅

---
+++ PopPang/Sources/App/PopPangApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopPangApp.swift
+//  PopPang
+//
+//  Created by 김동현 on 9/6/25.
+//
+
+import SwiftUI
+
+@main
+struct PopPangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
