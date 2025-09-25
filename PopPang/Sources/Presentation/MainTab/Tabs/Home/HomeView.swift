f7977029fc54b8ced531e2731be48eb14f840c97
indextrown <indextrown@gmail.com>
2025-09-25 17:14:22 +0900

    feat: AlertButton 구현

--- PopPang/Sources/Presentation/MainTab/Tabs/Home/HomeView.swift
+++ PopPang/Sources/Presentation/MainTab/Tabs/Home/HomeView.swift
@@ -11,11 +11,31 @@ struct HomeView: View {
     @State private var searchText = ""
     var body: some View {
         VStack {
-            SearchTextField(placeholder: "궁금한 장소를 검색해보세요", text: $searchText)
+            HStack(spacing: 0) {
+                SearchTextField(placeholder: "궁금한 장소를 검색해보세요", text: $searchText)
+                
+                AlertButton {
+                    
+                }
+                .padding(.leading, .contentPadding)
+                
+            }
             
             Spacer()
         }
         .padding(.contentPadding)
+    }
+}
+
+private struct AlertButton: View {
+    var action: () -> Void
+    
+    var body: some View {
+        Image("Bell")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 20, height: 20)
+            .padding(10)
     }
 }
 
