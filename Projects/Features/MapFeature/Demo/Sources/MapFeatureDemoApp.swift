b9296be3483205eb238accab1036351e49622eee
indextrown <indextrown@gmail.com>
2026-05-28 00:20:29 +0900

    feat: 기능 구현 변경 반영

--- Projects/Features/MapFeature/Demo/Sources/MapFeatureDemoApp.swift
+++ Projects/Features/MapFeature/Demo/Sources/MapFeatureDemoApp.swift
@@ -1,7 +1,17 @@
+import Data
+import Domain
+import MapFeature
 import SwiftUI
 
 @main
 struct MapFeatureDemoApp: App {
+    init() {
+        DIContainer.shared.register(
+            PopupUsecaseImpl(popupRepository: PopupRepositoryImpl()),
+            for: PopupUsecaseProtocol.self
+        )
+    }
+
     var body: some Scene {
         WindowGroup {
             MapFeatureView()
