26095e8f5c81ed8c6e30d2dedba27601148725f3
indextrown <indextrown@gmail.com>
2026-05-28 00:20:28 +0900

    chore: 프로젝트 설정 변경 반영

--- Projects/Features/FavoritesFeature/Project.swift
+++ Projects/Features/FavoritesFeature/Project.swift
@@ -41,20 +41,5 @@ let project = Project(
                 .project(target: "Data", path: "../../Data"),
             ]
         ),
-        .target(
-            name: "FavoritesFeatureTests",
-            destinations: [.iPhone],
-            product: .unitTests,
-            bundleId: "com.poppang.features.favorites.tests",
-            deploymentTargets: .iOS("17.0"),
-            infoPlist: .default,
-            sources: ["Tests/**"],
-            dependencies: [
-                .target(name: "FavoritesFeature"),
-                .project(target: "Core", path: "../../Shared/Core"),
-                .project(target: "Domain", path: "../../Domain"),
-                .project(target: "DSKit", path: "../../Shared/DSKit"),
-            ]
-        ),
     ]
 )
