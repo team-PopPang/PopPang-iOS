87c2fa75376694c32dbb15f7f05886da89c3996c
indextrown <indextrown@gmail.com>
2026-05-28 00:20:27 +0900

    feat: 기능 구현 변경 반영

--- Projects/Features/AlertFeature/Demo/Sources/AlertFeatureDemoApp.swift
+++ Projects/Features/AlertFeature/Demo/Sources/AlertFeatureDemoApp.swift
@@ -1,8 +1,21 @@
-import SwiftUI
 import AlertFeature
+import Data
+import Domain
+import SwiftUI
 
 @main
 struct AlertFeatureDemoApp: App {
+    init() {
+        DIContainer.shared.register(
+            PopupUsecaseImpl(popupRepository: PopupRepositoryImpl()),
+            for: PopupUsecaseProtocol.self
+        )
+        DIContainer.shared.register(
+            UserUsecaseImpl(userRepository: UserRepositoryImpl()),
+            for: UserUsecaseProtocol.self
+        )
+    }
+
     var body: some Scene {
         WindowGroup {
             AlertFeatureView()
