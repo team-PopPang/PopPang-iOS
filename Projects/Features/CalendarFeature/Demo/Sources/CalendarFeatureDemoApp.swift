0c64bd10067d8c4d3cacdc19661de23d3f9b7d30
indextrown <indextrown@gmail.com>
2026-05-27 11:36:22 +0900

    chore: 캘린더 데모 조립 갱신

--- Projects/Features/CalendarFeature/Demo/Sources/CalendarFeatureDemoApp.swift
+++ Projects/Features/CalendarFeature/Demo/Sources/CalendarFeatureDemoApp.swift
@@ -1,7 +1,17 @@
+import CalendarFeature
+import Data
+import Domain
 import SwiftUI
 
 @main
 struct CalendarFeatureDemoApp: App {
+    init() {
+        DIContainer.shared.register(
+            PopupUsecaseImpl(popupRepository: PopupRepositoryImpl()),
+            for: PopupUsecaseProtocol.self
+        )
+    }
+
     var body: some Scene {
         WindowGroup {
             CalendarFeatureView()
