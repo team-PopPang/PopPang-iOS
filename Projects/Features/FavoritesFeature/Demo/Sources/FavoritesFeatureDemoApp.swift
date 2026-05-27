b294ecb4fd8155806ba887d4a793877330ef8907
indextrown <indextrown@gmail.com>
2026-05-28 00:20:28 +0900

    feat: 기능 구현 변경 반영

--- Projects/Features/FavoritesFeature/Demo/Sources/FavoritesFeatureDemoApp.swift
+++ Projects/Features/FavoritesFeature/Demo/Sources/FavoritesFeatureDemoApp.swift
@@ -1,7 +1,17 @@
+import Data
+import Domain
+import FavoritesFeature
 import SwiftUI
 
 @main
 struct FavoritesFeatureDemoApp: App {
+    init() {
+        DIContainer.shared.register(
+            PopupUsecaseImpl(popupRepository: PopupRepositoryImpl()),
+            for: PopupUsecaseProtocol.self
+        )
+    }
+
     var body: some Scene {
         WindowGroup {
             FavoritesFeatureView()
