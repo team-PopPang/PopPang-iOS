84ecc93d47eb1944c184ec37795e7a7d5a4f91a9
indextrown <indextrown@gmail.com>
2026-05-28 00:20:32 +0900

    test: 테스트 코드 변경 반영

--- Projects/Shared/Core/Tests/Logging/LoggerTests.swift
+++ Projects/Shared/Core/Tests/Logging/LoggerTests.swift
@@ -2,7 +2,7 @@ import Testing
 @testable import Core
 
 struct LoggerTests {
-    @Test
+    @Test("Logger가 파일명과 함수 문맥을 포함한 메시지를 만든다")
     func loggerFormatsMessageWithFileAndFunctionContext() {
         let message = Logger.log(
             .debug,
