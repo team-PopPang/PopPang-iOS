308019b42f71438960856c688f6d4d5f3490e178
indextrown <indextrown@gmail.com>
2026-05-27 11:38:02 +0900

    chore: 프로필 데모 조립 갱신

--- Projects/Features/ProfileFeature/Demo/Sources/ProfileFeatureDemoApp.swift
+++ Projects/Features/ProfileFeature/Demo/Sources/ProfileFeatureDemoApp.swift
@@ -1,7 +1,17 @@
+import Data
+import Domain
+import ProfileFeature
 import SwiftUI
 
 @main
 struct ProfileFeatureDemoApp: App {
+    init() {
+        DIContainer.shared.register(
+            UserUsecaseImpl(userRepository: UserRepositoryImpl()),
+            for: UserUsecaseProtocol.self
+        )
+    }
+
     var body: some Scene {
         WindowGroup {
             ProfileFeatureView()
