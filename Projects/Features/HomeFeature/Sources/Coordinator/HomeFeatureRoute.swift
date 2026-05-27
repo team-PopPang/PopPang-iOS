7aedd91dbb21e85a50954fbcc7c1cf793ae1eba3
indextrown <indextrown@gmail.com>
2026-05-28 00:20:29 +0900

    refactor: 코디네이터 구조 변경 반영

--- Projects/Features/HomeFeature/Sources/Coordinator/HomeFeatureRoute.swift
+++ Projects/Features/HomeFeature/Sources/Coordinator/HomeFeatureRoute.swift
@@ -1,9 +1,34 @@
+import Domain
 import Foundation
 
 public enum HomeFeatureRoute: Hashable, Sendable {
-    case search
-    case popupDetail
-    case comingSoon
-    case alert
-    case review
+    case popupDetail(userUuid: String, popup: Popup)
+    case comingPopupDetail(userUuid: String, popups: [Popup])
+    case alert(userUuid: String)
+    case reviewDetail([Review])
+}
+
+public enum HomeSheetRoute: Identifiable, Hashable, Sendable {
+    case regionSheet
+    case sortSheet
+
+    public var id: String {
+        switch self {
+        case .regionSheet:
+            "regionSheet"
+        case .sortSheet:
+            "sortSheet"
+        }
+    }
+}
+
+public enum HomeFullScreenRoute: Identifiable, Hashable, Sendable {
+    case search(uuid: String)
+
+    public var id: String {
+        switch self {
+        case .search(let uuid):
+            "search-\(uuid)"
+        }
+    }
 }
