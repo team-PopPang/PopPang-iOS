a067226c83c04478b2a968f4323fb345751870d6
indextrown <indextrown@gmail.com>
2026-05-28 00:20:29 +0900

    feat: 기능 구현 변경 반영

--- Projects/Features/HomeFeature/Demo/Sources/HomeFeatureDemoApp.swift
+++ Projects/Features/HomeFeature/Demo/Sources/HomeFeatureDemoApp.swift
@@ -1,7 +1,16 @@
+import Data
+import Domain
+import HomeFeature
 import SwiftUI
 
 @main
 struct HomeFeatureDemoApp: App {
+    init() {
+        let popupRepository = PopupRepositoryImpl()
+        let popupUsecase = PopupUsecaseImpl(popupRepository: popupRepository)
+        DIContainer.shared.register(popupUsecase, for: PopupUsecaseProtocol.self)
+    }
+
     var body: some Scene {
         WindowGroup {
             HomeFeatureView()
